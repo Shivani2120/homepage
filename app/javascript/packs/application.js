93de3687f2487ee8f79cc0b0c71c39e8344b7a24
Shivani Shilpi <shivanishilpi2120@gmail.com>
2022-06-26 16:57:17 +0530

    home

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,44 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "@fortawesome/fontawesome-free/css/all"
+import 'bootstrap'; 
+
+require("jquery")
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+
+        var x=document.getElementById('login');
+		var y=document.getElementById('register');
+		var z=document.getElementById('btn');
+		function register()
+		{
+			x.style.left='-400px';
+			y.style.left='50px';
+			z.style.left='110px';
+		}
+		function login()
+		{
+			x.style.left='50px';
+			y.style.left='450px';
+			z.style.left='0px';
+		}
+	
+
+        var modal = document.getElementById('login-form');
+        window.onclick = function(event) 
+        {
+            if (event.target == modal) 
+            {
+                modal.style.display = "none";
+            }
+        }
+ 
